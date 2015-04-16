70218733eaab48ced482fa87b9b2f30ab57691c4
Reifier <apinchuk.soft@gmail.com>
2015-04-15 17:01:52 -0700

    Fixed the cursor disconnect caused by including database connection into a function.

--- AssetConverter.py
+++ AssetConverter.py
@@ -7,6 +7,8 @@ import logging
 import MySQLdb as db
 import mysql.connector as connector
 import config
+import csv
+from datetime import datetime
 
 #This is a file that is to be converted into a database
 CSV_PATH = sys.argv[1]
@@ -45,7 +47,28 @@ def create_database(cursor):
 
 create_database(CURSOR)
 
+
+
 CURRENT_LINE = CSV_FILE.readline()
 logging.info('The current line: %s', CURRENT_LINE)
-LIST_OF_VALUES = CURRENT_LINE.split()
-print LIST_OF_VALUES
+#Parse the CSV file
+f = open(CSV_PATH, 'rt')
+reader = csv.reader(f)
+CURRENT_LINE = reader.next()
+CURRENT_LINE = reader.next()
+print CURRENT_LINE
+
+print CURRENT_LINE[6]
+date = datetime.strptime(CURRENT_LINE[6], '%B  %d, %Y').date()
+print date
+
+
+date = {'date': datetime.strptime(CURRENT_LINE[6], '%B  %d, %Y').date()}
+
+print date['date']
+
+
+def string_to_date(string):
+    """Turn string object into date"""
+    date_to_return = datetime.strptime(string, '%B  %d, %Y').date()
+    return date_to_return
