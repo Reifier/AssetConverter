d99f87ee26f2bb3913adead578735fbb65ed7350
Reifier <apinchuk.soft@gmail.com>
2015-04-21 16:36:25 -0700

    Polishing things up

--- AssetConverter.py
+++ AssetConverter.py
@@ -1,13 +1,14 @@
 #!/usr/bin/evn python2
 
-__author__ = 'apinchuke'
+__author__ = 'apinchuk'
 
 import sys
 import logging
 import mysql.connector as connector
+from datetime import datetime
 import config
 import csv
-from datetime import datetime
+
 
 # This is a file that is to be converted into a database
 CSV_PATH = sys.argv[1]
@@ -29,7 +30,7 @@ CONNECTION = None
 
 logging.debug('The user to be logged in: %s', config.database['user'])
 
-
+# Login into MySQL check if the database exists, and create it if it does not.
 CONNECTION = connector.connect(**config.database)
 CURSOR = CONNECTION.cursor()
 logging.debug('The current DB user is: %s', CONNECTION.user)
@@ -43,8 +44,9 @@ def create_database(cursor):
         print("Failed creating database: {}".format(err))
         exit(1)
 
-
 create_database(CURSOR)
+
+# Connect to the actual database (existed or created)
 CONNECTION = connector.connect(user=config.database['user'], database=DB_NAME)
 CURSOR = CONNECTION.cursor()
 
@@ -53,22 +55,13 @@ logging.info('The current line: %s', CURRENT_LINE)
 # Parse the CSV file
 CSV_FILE = open(CSV_PATH, 'rU')
 reader = csv.reader(CSV_FILE)
-CURRENT_LINE = reader.next()
-CURRENT_LINE = reader.next()
+next(reader) # Skip two first header lines
+next(reader)
 
+
+# Remove all the newlines and tabs from the file
 new_line_filtered = (line.replace('\n', ' ') for line in CSV_FILE)
 tab_filtered = (line.replace('\t', ' ') for line in new_line_filtered)
-
-for row in csv.reader(tab_filtered):
-    print row
-
-print CURRENT_LINE
-
-print CURRENT_LINE[6]
-date = datetime.strptime(CURRENT_LINE[6], '%B  %d, %Y')
-print date.date()
-
-
 
 insert_statement = ("INSERT INTO available_assets"
                     "(name, "
@@ -84,35 +77,20 @@ insert_statement = ("INSERT INTO available_assets"
                     "location, "
                     "allocated_to)"
                     "VALUES"
-                    "(%(name)s, "
-                    "%(model)s, "
-                    "%(asset_id)s, "
-                    "%(description)s, "
-                    "%(created_by)s, "
-                    "%(purchased_on)s, "
-                    "%(price_$)s, "
-                    "%(group)s, "
-                    "%(sub_group)s, "
-                    "%(vendor)s, "
-                    "%(location)s, "
-                    "%(allocated_to)s)")
+                    "(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)")
 
-insert_values = {'name': CURRENT_LINE[1],
-                 'model': CURRENT_LINE[2],
-                 'asset_id': CURRENT_LINE[3],
-                 'description': CURRENT_LINE[4],
-                 'created_by': CURRENT_LINE[5],
-                 'purchased_on': date.date(),
-                 'price_$': CURRENT_LINE[7],
-                 'group': CURRENT_LINE[8],
-                 'sub_group': CURRENT_LINE[9],
-                 'vendor': CURRENT_LINE[10],
-                 'location': CURRENT_LINE[11],
-                 'allocated_to': CURRENT_LINE[12]}
 
-print insert_values
 
-CURSOR.execute(insert_statement, insert_values)
+
+for CURRENT_LINE in csv.reader(tab_filtered):
+    # Convert the date from string into appropriate MySQL format
+    CURRENT_LINE[6] = datetime.strptime(CURRENT_LINE[6], '%B  %d, %Y')
+    insert_values = (CURRENT_LINE[1], CURRENT_LINE[2], CURRENT_LINE[3], CURRENT_LINE[4], CURRENT_LINE[5],
+                     CURRENT_LINE[6], CURRENT_LINE[7], CURRENT_LINE[8], CURRENT_LINE[9], CURRENT_LINE[10],
+                     CURRENT_LINE[11], CURRENT_LINE[12])
+    print insert_values
+    CURSOR.execute(insert_statement, insert_values)
+
 CONNECTION.commit()
 CURSOR.close()
 CONNECTION.close()
